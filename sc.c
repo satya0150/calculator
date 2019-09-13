085b899a42a87e0797a35e6d22812ab0ef028a26
satya0150 <55277495+satya0150@users.noreply.github.com>
2019-09-13 15:22:02 +0530

    Create sc.c
    initial commit

---
+++ sc.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ int a,b;
+ char c,d;
+ a:
+ printf("what do u want to do\n");
+ scanf(" %c", &c);
+ printf("enter the first value\n");
+ scanf("%i", &a);
+ printf("enter the second value\n");
+ scanf("%i", &b);
+ printf("result is ");
+ if(c=='+'){
+    printf("%i",a+b);
+ }
+ else if(c=='-'){
+    printf("%i",a-b);
+ }
+ else if(c=='*'){
+    printf("%i",a*b);
+ }
+ else if(c=='/'){
+    printf("%i",a/b);
+ }
+ else
+    printf("error");
+ printf("\ndo you want calculate again\n");
+ scanf(" %c", &d);
+ if(d=='y'||d=='Y'){
+    goto a;
+ }
+ else
+    exit(0);
+}
